7f7fd6e41a101cdae93daadeef94cd0a3e483730
Connor Wycherley <connorwycherley@Connors-MacBook-Pro.local>
2019-01-28 14:34:54 +0000

    Refactored to use a helper file. Data structures moved to config file

--- functions/index.js
+++ functions/index.js
@@ -1,68 +1,23 @@
 const firebase = require("firebase-admin");
 const functions = require('firebase-functions');
-const serviceAccount = require("./key.json");
-const firebaseDb = require("./firebase-db.json");
-
-// const interpreterListingData = ["FIRST_NAME","LAST_NAME"];
-const interpreterListingData = ["FIRST_NAME","LAST_NAME","GENDER","MENTAL_HEALTH_WORK","LEGAL_WORK","IMG_URL"];
+const core = require("./ybsli-firebase-core");
+const inrerpreterDataFields = require("./ybsli-interpreter-data");
 
 
 exports.getInterpreterListing = functions.https.onRequest((req, res) => {
-  returnData()
+  const interpreterListingData = inrerpreterDataFields.interpreterListingData;
+  core.returnData()
   .then(function(data) {
-    data = selectFields(data, interpreterListingData);
+    // console.info("retrived data");
+    data = core.selectFields(data, interpreterListingData);
     res.send(data);
-  }, function(error){
-    res.status(error).send();
+  }, function(error) {
+    // console.info("could not retrieve data");
+    console.info(error);
+    res.status(500).send();
   })
-  .then(function(){cleanUp()})
+  .then(function(){
+    // console.info("cleaning up connection");
+    core.cleanUp()
+  })
 });
-
-// Attach an asynchronous callback to read the data at our posts reference
-function returnData() {
-    console.info("Getting data")
-    return new Promise (function(resolve, reject) {
-      firebase.initializeApp({
-      credential: firebase.credential.cert(serviceAccount),
-      databaseURL: firebaseDb.databaseURL
-    });
-    console.info("Initialising database")
-    const db = firebase.database();
-    const ref = db.ref("firebaseDb.collectionName");
-
-    ref.once("value", function(snapshot) {
-      resolve(snapshot.toJSON());
-    }, function (errorObject) {
-      reject(errorObject.code);
-    });
-  });
-}
-
-function selectFields(data, fields) {
-  var arr = [];
-  const keys = Object.keys(data);
-  for (obj in data) {
-    const localObj = data[obj]
-    var tempObj = {};
-    tempObj.KEY = keys[obj];
-    for (field in fields) {
-      const localField = fields[field];
-      tempObj[localField] = localObj[localField];
-    }
-    arr.push(tempObj);
-  }
-  return arr;
-}
-
-function filterByValue(item) {
-  for (var key in filter) {
-    if (item[key] === undefined || item[key] != filter[key]) {
-      return false;
-    }
-  }
-  return true;
-}
-
-function cleanUp() {
-  firebase.app().delete();
-}
